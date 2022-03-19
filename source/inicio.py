ae7fa868ea257277718e4d6c132b8329ab3b3453
jalopezqv <jalopezqvision@gmail.com>
2022-03-19 11:56:33 -0500

    Se agregan los campos de cedula y los radio button para seleccionar sexo, tambien se valida que la cedula no se encuentre registrada, falta validar que la cedula no este vacia y que solo permita numeros.

--- source/inicio.py
+++ source/inicio.py
@@ -2,13 +2,29 @@
 Funcionalidad vista de inicio
 """
 
+from operator import truediv
 from tkinter import *
+from tkinter import messagebox
+
+lista_votantes = []
+
+# validar si el votante ya ejercio su voto
+def validar_votante(cedula):
+        
+    if cedula in lista_votantes:
+        messagebox.showinfo(message="El documento ingresado ya se encuentra registrado, tenga en cuenta que solo puede votar 1 vez", title="ERROR")
+        return False
+    return True
 
 # conectar con vista de votacion y configurar el estado de los botones 
-def show_votacion():
+def show_votacion(cedula, sexo_votante):
     try:
         import votacion
     except ImportError:
         import sys
         votacion = sys.modules[__package__ + '.votacion']
+
+    datos_votante = [cedula, sexo_votante]
+    lista_votantes.append(datos_votante)
+
     votacion.func_states(NORMAL,3)
--- views/inicio.py
+++ views/inicio.py
@@ -2,6 +2,7 @@
 Vista de inicio
 """
 
+import opcode
 from tkinter import *
 from .vot import *
 
@@ -29,9 +30,16 @@ def inicio():
     # --------- connections ----------
 
     def s_votacion():
-        inicio.destroy()
-        show_votacion()
-        
+        cedula_votante = txt_cedula_identidad.get()
+        sexo_votante   = opcion.get()
+        if validar_votante(cedula_votante):
+            inicio.destroy()
+            show_votacion(cedula_votante, sexo_votante)
+
+        txt_cedula_identidad.delete(0,END)
+        opcion.set(None)
+
+      
     # ---------- Headboard -----------
 
     # img elecciones 2022
@@ -40,12 +48,16 @@ def inicio():
 
     h1 = Label(inicio, text='ELECCIONES CALI 2022', bg='white', font=('helvetica', 14))
 
+    # ---------- Entrys ------------
+    txt_cedula_identidad = Entry(inicio, width=10, text='Ingrese su cedula de identificacion ... ')
+    
+    # ---------- RadioButtons ------------
+    opcion = IntVar()
+    rbt_masculino = Radiobutton(inicio, text='Masculino', variable=opcion, value=1)
+    rbt_femenino  = Radiobutton(inicio, text='Femenino',  variable=opcion, value=2)
 
     # ---------- buttons ------------
-
-
-
-    votar = Button(inicio, text='VOTAR', command=s_votacion)
+    btn_votar = Button(inicio, text='VOTAR', command=s_votacion)
 
     # ----- llamados y ubicaciones -----
 
@@ -55,5 +67,15 @@ def inicio():
     lbl_img1.pack()
     lbl_img1.place(x=90, y=7)
 
-    votar.pack()
+
+    txt_cedula_identidad.pack()
+    txt_cedula_identidad.place(x=375, y= 270)
+
+    rbt_masculino.pack()
+    rbt_masculino.place(x=450, y=270)
+    rbt_femenino.pack()
+    rbt_femenino.place(x=550, y=270)
+
+    btn_votar.pack()
+    btn_votar.place(x=375, y=300)
     inicio.mainloop()
